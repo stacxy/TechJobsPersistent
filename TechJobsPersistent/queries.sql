a0447b18b6477f08027cfc579cd5c4d2742b7098
stacxy <72173457+stacxy@users.noreply.github.com>
2021-02-15 18:28:15 -0600

    Update queries.sql

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -2,13 +2,14 @@
 select column_name, data_type
 from information_schema.columns
 where table_name = 'jobs';
-select *
-from jobs;
 
 #--Part 2
 SELECT name 
 FROM employers
-WHERE Location = "St. Louis";
+WHERE Location = "Saint Louis";
 
 #--Part 3
-
+SELECT Name, Description
+from techjobs.skills
+INNER JOIN jobskills ON skills.id = jobskills.SkillId
+ORDER BY Name;
